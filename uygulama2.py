063cf4147a623a0f3ceaf67666def023be71523e
FatmanurSahin08 <fatmanursahin2003@gmail.com>
2024-08-01 16:45:12 +0300

    first commit

---
+++ uygulama2.py
@@ -0,0 +1,21 @@
+import math
+
+points = [(1, 2), (4, 6), (5, 1), (3, 3)]
+
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        dist = euclideanDistance(points[i], points[j])
+        distances.append(dist)
+        print(f"Mesafe {points[i]} ve {points[j]} arasında: {dist}")
+
+if distances:
+    
+    min_distance = min(distances)
+    print("Noktalar arasındaki minimum mesafe:", min_distance)
+else:
+    print("Hesaplanacak mesafe bulunamadı.")
